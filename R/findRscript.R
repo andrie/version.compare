2e9076cc88165fcce449ecdd627f38af967d65a5
Andrie de Vries <apdevries@gmail.com>
2015-07-17 15:34:56 +0100

    Fix problems in the way findRscript() passes arguments

--- R/findRscript.R
+++ R/findRscript.R
@@ -8,31 +8,36 @@
 #' This function is a wrapper around \code{list.files} setting the search pattern to \code{Rscript.exe}.
 #'
 #' @param path Character vector of search paths. Passed to \code{\link{list.files}}
+#' @param executable Regular expression that defines the name of the Rscript executable. Defaults to a regular expression that finds \code{Rscript} on linux machines and \code{Rscript.exe} on Windows
 #' @param pattern regular expression.  Passed to \code{\link{list.files}}
+#' @param version Additional regular expression, e.g. "3.2.1" to limit the results list to a specific version of R
 #'
 #' @export
-findRscript <- function(path, pattern){
-  foo <- function(path, pattern, recursive){
+findRscript <- function(path, executable = "(Rscript$|Rscript.exe)", pattern, version){
+
+  foo <- function(path, executable, recursive){
     message("Now searching ", path)
-    list.files(path=path, pattern=pattern, full.names=TRUE, include.dirs=TRUE, recursive=recursive)
+    list.files(path=path, pattern=executable, full.names=TRUE, include.dirs=TRUE, recursive=recursive)
   }
-  switch(Sys.info()[["sysname"]],
-         Linux = {
-           paths <- c(
-             "/usr/lib64",
-             "/usr/lib"
-           )
-           ret <- sapply(paths, foo, pattern="Rscript$", recursive=TRUE)
-         },
-         Windows = {
-           paths <- c("c:/program files", "c:/program files (x86)", "c:/R", "c:/revolution")
-           ret <- sapply(paths, foo, pattern="Rscript.exe$", recursive=TRUE)
-         },{
-           paths <- c(
-             "/Library/Frameworks/"
-           )
-           ret <- sapply(paths, foo, pattern="Rscript.exe$", recursive=TRUE)
-         }
-  )
-  unname(unlist(ret))
+
+  if(missing(path) || is.null(path)){
+    path <- switch(Sys.info()[["sysname"]],
+                   Linux = c("/usr/lib64", "/usr/lib"),
+                   Windows = c("c:/program files", "c:/program files (x86)", "c:/R", "c:/revolution", "c:/rro"),
+                   c("/Library/Frameworks/")
+    )
+  }
+
+  if(missing(executable) || is.null(executable)){
+    executable <- switch(Sys.info()[["sysname"]],
+                      Linux = "Rscript$",
+                      Windows = "Rscript.exe$",
+                      "Rscript$"
+    )
+  }
+
+  ret <- lapply(path, foo, executable=executable, recursive=TRUE)
+  ret <- unname(unlist(ret))
+  if(!missing("version") && !is.null(version)) ret <- ret[grepl(version, ret)]
+  normalizePath(ret)
 }
