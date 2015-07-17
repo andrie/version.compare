ed57f79f0f6175448f97101df1fc98db172c519a
Andrie de Vries <apdevries@gmail.com>
2015-07-17 15:35:54 +0100

    Modify examples to find instances of R with the same version of current session

--- inst/examples/example_version.time.R
+++ inst/examples/example_version.time.R
@@ -1,21 +1,10 @@
 # Find installed versions of Rscript
 
-rscript <- findRscript()
+rversion <- paste(R.version$major, R.version$minor, sep = ".")
+rscript <- findRscript(version = rversion)
 
 
 # Configure which installed version to use
-
-rscript <- switch(
-  Sys.info()[["sysname"]],
-  Windows = c(
-    "c:/program files/RRO/R-3.1.1/bin/x64/Rscript.exe",
-    "c:/program files/R/R-3.1.1/bin/x64/Rscript.exe"
-  ),
-  Linux = c(
-    "/usr/lib64/RRO-8.0/R-3.1.1/lib/R/bin/Rscript",
-    "/usr/lib/R/bin/Rscript"
-  )
-)
 
 # Compute vector mean in different R installations
 
