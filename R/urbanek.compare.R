55f667520d30cae39e582545d8a6fde47ef06cf7
Andrie de Vries <apdevries@gmail.com>
2015-07-20 21:14:36 +0100

    Bug fixes and documentation fixes

--- R/urbanek.compare.R
+++ R/urbanek.compare.R
@@ -1,16 +1,18 @@
 #' Runs Urbanek tests on multiple versions of R and compares results.
 #'
-#' @inheritParams version.compare
+#' @inheritParams version.time
 #' @inheritParams urbanek2.5
 #'
 #' @export
 urbanek.compare <- function(rVersions, threads = 4, scale.factor = 1){
+
+  paste.deparse <- function(x, collapse = "\n") paste(deparse(x), collapse = collapse)
+
   tf <- tempfile(pattern = "urbanek-", fileext = ".r")
-  code <- paste("urbanek2.5 <- ", paste(deparse(urbanek2.5), collapse = "\n"))
-  fn <- sprintf("urbanek2.5(threads = %s, scale.factor = %s)", threads, scale.factor)
+  code <- paste("urbanek2.5 <- ", paste.deparse(urbanek2.5))
+  fn <- sprintf("urbanek2.5(threads = %s, scale.factor = %s)", paste.deparse(threads, collapse = " "), scale.factor)
   code <- paste(code, fn, sep = "\n")
   write(code, file = tf)
   on.exit(unlink(tf))
-  res <- version.time(rVersions, file = tf)
-  res
+  version.time(rVersions, file = tf)
 }
--- R/version.time.R
+++ R/version.time.R
@@ -1,7 +1,8 @@
 #' Run script in different versions of R and capture system time.
 #'
-#' @param expr Expression
 #' @param rVersions Character vector containing path to Rscript in different installations of R
+#' @param expr Expression to evaluate
+#' @param file If specified, evaluates the code in the file, rather than \code{expr}
 #' @export
 #'
 #' @example \inst\examples\example_version.time.R
@@ -10,6 +11,7 @@ version.time <- function(rVersions, expr, file){
   expr <- match$expr
 
   # capture system.time of original expression and write results to tempfile
+  p0 <- "library('methods')"
   p1 <- "result <- local({"
   p2 <- "})
     tf <- normalizePath(tempfile(fileext = '.rds', tmpdir = dirname(tempdir())), mustWork = FALSE)
@@ -26,7 +28,7 @@ version.time <- function(rVersions, expr, file){
   } else {
     tmp <- paste(deparse(expr), collapse = "\n")
   }
-  new.expr <- sprintf("%s\n%s\n%s", p1, tmp, p2)
+  new.expr <- sprintf("%s\n%s\n%s\n%s", p0, p1, tmp, p2)
 
   # save new expression to file so it can be picked up by clean R session
   write(new.expr, file = scriptfile)
@@ -37,18 +39,19 @@ version.time <- function(rVersions, expr, file){
 
   # run the actual script in each R version
   res <- lapply(rVersions, runWithRscript, testscriptPath = scriptfile)
+  result.tempfiles = sapply(res, function(x)tail(x[["stdout"]], 1))
+  mres <- list(
+    time = lapply(res, function(x)x[["time"]]),
+    stdout = lapply(res, function(x)head(x[["stdout"]], -1))
+  )
+
+  res <- NULL
 
   # retrieve results stored in tempfiles
-  result.tempfiles <- sapply(res, function(x)tail(x$stdout, 1))
   on.exit(unlink(result.tempfiles))
-  version.results <- lapply(result.tempfiles, readRDS)
+  mres[["version.results"]] <- lapply(result.tempfiles, readRDS)
 
-  # append script results to each component
-  for(i in seq_along(result.tempfiles)){
-    res[[i]]$results <- version.results[[i]]
-    res[[i]]$stdout <- head(res[[i]]$stdout, -1)
-  }
-  res
+  mres
 }
 
 
