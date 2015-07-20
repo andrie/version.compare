ccb54f7ece4eeb18b5d387a1c8c0d2a1e48c9b87
Andrie de Vries <apdevries@gmail.com>
2015-07-20 14:27:45 +0100

    Add ability to search Windows registry for installed versions

--- R/findRscript.R
+++ R/findRscript.R
@@ -1,3 +1,17 @@
+# Searches windows registry for installed versions of R
+findRinRegistry <- function(){
+  readOnce <- function(ptn){
+    x <- readRegistry(ptn, maxdepth = 3)
+    if(length(x)){
+      y <- unlist(x, recursive = TRUE)
+      unname(y[grep(".InstallPath", names(y))])
+    } else NULL
+  }
+  patterns <- c("SOFTWARE\\Revolution", "SOFTWARE\\R-core")
+  unique(unname(unlist(sapply(patterns, readOnce))))
+}
+
+
 
 #' Returns list of R installation paths.
 #'
@@ -7,7 +21,7 @@
 #' @details
 #' This function is a wrapper around \code{list.files} setting the search pattern to \code{Rscript.exe}.
 #'
-#' @param path Character vector of search paths. Passed to \code{\link{list.files}}
+#' @param path Character vector of search paths. Passed to \code{\link{list.files}}. If not specified (the default) then the search path contains typical installation paths for different operating systems.
 #' @param executable Regular expression that defines the name of the Rscript executable. Defaults to a regular expression that finds \code{Rscript} on linux machines and \code{Rscript.exe} on Windows
 #' @param pattern regular expression.  Passed to \code{\link{list.files}}
 #' @param version Additional regular expression, e.g. "3.2.1" to limit the results list to a specific version of R
@@ -16,14 +30,15 @@
 findRscript <- function(path, executable = "(Rscript$|Rscript.exe)", pattern, version){
 
   foo <- function(path, executable, recursive){
-    message("Now searching ", path)
+    # message("Now searching ", path)
     list.files(path=path, pattern=executable, full.names=TRUE, include.dirs=TRUE, recursive=recursive)
   }
 
   if(missing(path) || is.null(path)){
     path <- switch(Sys.info()[["sysname"]],
                    Linux = c("/usr/lib64", "/usr/lib"),
-                   Windows = c("c:/program files", "c:/program files (x86)", "c:/R", "c:/revolution", "c:/rro"),
+                   # Windows = c("c:/program files", "c:/program files (x86)", "c:/R", "c:/revolution", "c:/rro"),
+                   Windows = sort(unique(dirname(findRinRegistry()))),
                    c("/Library/Frameworks/")
     )
   }
