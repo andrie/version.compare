945b40f25f6d26f7b59f6e5f8f2ea68b864c94c8
andrie <apdevries@gmail.com>
2016-02-12 09:44:40 +0000

    Rebuild vignette

--- inst/doc/version.compare.R
+++ inst/doc/version.compare.R
@@ -9,12 +9,11 @@ scale.factor <- 0.5
 r <- findRscript(version = "3.2.3.*x64")
 test.results <- RevoMultiBenchmark(rVersions = r, threads = c(1, 4), scale.factor = scale.factor)
 
-## ----compare-results, echo=FALSE-----------------------------------------
+## ----compare-results, echo = FALSE, fig.width=6, fig.height=3, out.width="800px", dpi=1200----
 kable(test.results)
+plot(test.results, theme_size=8, main = "Elapsed time")
 
-## ----performance-speed-up, echo=FALSE------------------------------------
+## ----performance-speed-up, echo = FALSE, fig.width=6, fig.height=3, out.width="800px", dpi=1200----
 kable(urbanekPerformance(test.results), digits = 2)
-
-## ----plot-results, echo = FALSE, fig.width=6, fig.height=3, out.width="800px", dpi=600----
-plot(test.results, theme_size=10)
+plot(urbanekPerformance(test.results), theme_size=8, main = "Relative Performance")
 
