df48d1eb0d3111265fdaa4dba6a909626c235915
andrie <apdevries@gmail.com>
2016-04-14 21:36:04 +0100

    Re-build vignette

--- inst/doc/version.compare.R
+++ inst/doc/version.compare.R
@@ -1,19 +1,27 @@
-## ----init, include=FALSE-------------------------------------------------
+## ------------------------------------------------------------------------
 library("version.compare")
 library("knitr")
+scale.factor <- 1.0
 
-## ------------------------------------------------------------------------
-scale.factor <- 0.5
+## ----benchmark, echo=TRUE------------------------------------------------
+r <- findRscript(version = "3.2.4.*x64")
+test.results <- RevoMultiBenchmark(rVersions = r, 
+                                   threads = c(1, 4, 8), 
+                                   scale.factor = scale.factor)
 
-## ----benchmark, echo=FALSE-----------------------------------------------
-r <- findRscript(version = "3.2.3.*x64")
-test.results <- RevoMultiBenchmark(rVersions = r, threads = c(1, 4), scale.factor = scale.factor)
+## ----compare-results, eval = FALSE---------------------------------------
+#  kable(test.results)
+#  plot(test.results, theme_size = 8, main = "Elapsed time")
 
-## ----compare-results, echo = FALSE, fig.width=6, fig.height=3, out.width="800px", dpi=1200----
+## ----performance-speed-up, eval = FALSE----------------------------------
+#  kable(urbanekPerformance(test.results), digits = 2)
+#  plot(urbanekPerformance(test.results), theme_size = 8, main = "Relative Performance")
+
+## ----compare-results, echo = FALSE, fig.width=6, fig.height=3, out.width="800px", dpi=1200, keep=TRUE----
 kable(test.results)
-plot(test.results, theme_size=8, main = "Elapsed time")
+plot(test.results, theme_size = 8, main = "Elapsed time")
 
 ## ----performance-speed-up, echo = FALSE, fig.width=6, fig.height=3, out.width="800px", dpi=1200----
 kable(urbanekPerformance(test.results), digits = 2)
-plot(urbanekPerformance(test.results), theme_size=8, main = "Relative Performance")
+plot(urbanekPerformance(test.results), theme_size = 8, main = "Relative Performance")
 
